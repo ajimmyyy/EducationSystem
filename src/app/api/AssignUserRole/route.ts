cefc06bacea5d8a4a0b5874c644803233a42f193
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2023-12-19 19:58:16 +0800

    feat: 完成分配使用者職位api
    分配使用者(teacher, student, manager)職位

--- src/app/api/AssignUserRole/route.ts
+++ src/app/api/AssignUserRole/route.ts
@@ -1,12 +1,9 @@
 import { assignUserRoleCase} from "./module";
-import { CreateStudentType } from "./module";
-import { CreateTeacherType } from "./module";
-import { CreateManagerType } from "./module";
 
 interface RequestBody {
     role: string;
     userid: number;
-    body: Record<string, any>;
+    body: any;
 }
 
 export async function POST(request: Request) {
@@ -14,45 +11,42 @@ export async function POST(request: Request) {
     if (!body.role || !body.userid || !body.body) {
         throw new Error('Invalid request. Missing required fields.');
     }
-    
+
+    try{
+        if(!await assignUserRoleCase.IsAssignable(body.userid)){
+            return Response.json({ success: false, error: "had role" })
+        }
+    }
+    catch (error) {
+        return Response.json({ success: false, error })
+    }
+
     switch (body.role) {
         case 'student':
-            let studentBody: CreateStudentType;
-
             try {
-                studentBody = body.body as CreateStudentType;
+                const user = await assignUserRoleCase.AssignStudentRole(body.userid, body.body);
+                return Response.json({ success: true, user })
             }
             catch (error) {
                 return Response.json({ success: false, error })
             }
-
-            await assignUserRoleCase.AssignStudentRole(body.userid, studentBody);
-            break;
         case 'teacher':
-            let teacherBody: CreateTeacherType;
-
             try {
-                teacherBody = body.body as CreateTeacherType;
+                const user = await assignUserRoleCase.AssignTeacherRole(body.userid, body.body);
+                return Response.json({ success: true, user })
             }
             catch (error) {
                 return Response.json({ success: false, error })
             }
-
-            await assignUserRoleCase.AssignTeacherRole(body.userid, teacherBody);
-            break;
         case 'manager':
-            let managerBody: CreateManagerType;
-
             try {
-                managerBody = body.body as CreateManagerType;
+                const user = await assignUserRoleCase.AssignManagerRole(body.userid, body.body);
+                return Response.json({ success: true, user })
             }
             catch (error) {
                 return Response.json({ success: false, error })
             }
-
-            await assignUserRoleCase.AssignManagerRole(body.userid, managerBody);
-            break;
         default:
-            throw new Error('Unsupported request type.');
+            return Response.json({ success: false, error: "role doesn't exist"})
       }
 }
