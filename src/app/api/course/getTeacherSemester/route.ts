02fb76027e0a6143722a04725c28f7673a6663e8
Syuan-He <113964998+Syuan-He@users.noreply.github.com>
2023-12-30 00:03:42 +0800

    增加找到TeacherSemester的API

---
+++ src/app/api/course/getTeacherSemester/route.ts
@@ -0,0 +1,17 @@
+import { getTeacherSemester } from "@/services/teacherSemester";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest){
+    const params = await request.nextUrl.searchParams;
+    const teacherID = params.get("teacherID") || {};
+
+    if (!teacherID) {
+        return Response.json({ success: false, error: 'Invalid request. Missing teacherID. ' + teacherID });
+    }
+    try{
+        const courseSemester = await getTeacherSemester(Number(teacherID));
+        return Response.json({ success: true, courseSemesters: courseSemester });
+    }catch(error){
+        return Response.json({ success: false, error })
+    }
+}
