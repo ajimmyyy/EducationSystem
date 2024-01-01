ef93feccfe8a02aa0aa34234e2abd0eea9f4d094
night2021-2021 <thatnight2021@gmail.com>
2024-01-01 18:21:05 +0800

    添加退選彈跳視窗

--- src/components/EnrollCourse/EnrollcourseTable.tsx
+++ src/components/EnrollCourse/EnrollcourseTable.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Typography, Button } from "@material-tailwind/react";
 import { MdCreate } from "react-icons/md";
+import AlertWindows from './AlertWindowsWithDraw';
+import { ZodNumberCheck } from 'zod';
 
 const TABLE_HEAD = ["State", "Course Name", "Course ID", ""];
 const studentId = 748; // 固定的學生ID
@@ -8,7 +10,24 @@ const semester = "112-1"; // 固定的學期
 
 export function DefaultTable() {
   const [courses, setCourses] = useState([]);
+  const [isEditAlertOpen, setIsEditAlertOpen] = useState(false);
+  const [editAlertMessage, setEditAlertMessage] = useState('');
 
+  const openEditAlert = (courseCode: string, courseName: string) => {
+    const message = `是否要退選課程 [${courseCode}]${courseName}？`;
+    setIsEditAlertOpen(true);
+    setEditAlertMessage(message);
+  };
+
+  const closeEditAlert = () => {
+    setIsEditAlertOpen(false);
+  };
+
+  const handleDelete = async (courseCode : string) => {
+    // 實現刪除課程的邏輯
+    closeEditAlert();
+  };
+  
   useEffect(() => {
     const fetchCourses = async () => {
       try {
@@ -71,19 +90,6 @@ export function DefaultTable() {
                   </Typography>
                 </td>
 
-                {/* <td className={classes}>
-                  <div className="bg-blue-gray-100 rounded-full px-2 py-1 inline-block">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                      placeholder={courseTime}
-                    >
-                      {courseTime}
-                    </Typography>
-                  </div>
-                </td> */}
-
                 <td className={classes}>
                   <Typography
                     variant="small"
@@ -96,13 +102,17 @@ export function DefaultTable() {
                 </td>
 
                 <td className={classes} style={{ width: '150px' }}>
-                  <Button size="sm" variant="gradient" color="blue-gray" className="flex items-center gap-2" placeholder= "">修改<MdCreate /></Button>
+                  <Button size="sm" variant="gradient" color="blue-gray" className="flex items-center gap-2" onClick={() => openEditAlert(courseCode, courseName)} placeholder="">
+                    修改<MdCreate />
+                  </Button>
                 </td>
               </tr>
+              
             );
           })}
         </tbody>
       </table>
+      <AlertWindows isOpen={isEditAlertOpen} message={editAlertMessage} onClose={closeEditAlert} onConfirm={() => handleDelete(editAlertMessage.split(' ')[3])} />
     </Card>
   );
 }
